9e2e8ba7b9b1df35e4abbb72cd16fdf138eb1917
Toby Batch <tobias@neontribe.co.uk>
2017-05-13 09:53:30 +0100

    Cleaned up docker image and inlined startup scripts

--- cove/cove.startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/nginx restart
-cd /opt/cove
-export DJANGO_SETTINGS_MODULE=cove_iati.settings
-python3 manage.py migrate
-python3 manage.py compilemessages
-python3 manage.py runserver
-
