43c9b91c5292befe45b3404d285026b2efe40420
Toby Batch <tobias@neontribe.co.uk>
2017-07-24 10:27:59 +0100

    Added support to start the webserver as well as process cli inout

--- cove/startup.sh
+++ cove/startup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "$(</dev/stdin)" > /tmp/file
-FILEPATH=$(python manage.py upload /tmp/file)
-BASENAME=$(/usr/bin/basename $FILEPATH)
-XML=/opt/cove/media/${BASENAME}/file
-ERR=/opt/cove/media/${BASENAME}/*.json
-/bin/cat $XML
-(>&2 /bin/cat $ERR)
+if [ ! -z "$PROCESS_DATA" ]; then
+    /usr/local/bin/process.sh
+    exit $?
+fi
+
+/usr/local/bin/update.sh
+cd /opt/cove
+export DJANGO_SETTINGS_MODULE=cove_iati.settings
+python3 manage.py migrate
+python3 manage.py compilemessages
+python3 manage.py runserver
