56ecd8c453258f1b957340be3f7c5e914838d6ef
Toby Batch <tobias@neontribe.co.uk>
2017-09-14 20:49:59 +0100

    Testing geocoder

--- dockers/geocoder/process.sh
+++ dockers/geocoder/process.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "$(</dev/stdin)" > /tmp/file
-FILEPATH=$(python manage.py upload /tmp/file)
-BASENAME=$(/usr/bin/basename $FILEPATH)
-XML=/opt/cove/media/${BASENAME}/file
-ERR=/opt/cove/media/${BASENAME}/*.json
-if [ -e "$XML" ]; then
-	/bin/cat $XML
-fi
-if [ -e "" ]; then
-	(>&2 /bin/cat )
-fi
+# python3 src/main.py -c geocode -f example.pdf  -tGN && cat out.tsv
 
+echo "$(</dev/stdin)" > /tmp/file
+cat /tmp/file
+
+# cd /opt/geocoder-ie
+# python3 src/main.py -c geocode -f /tmp/file -t${country} > out.txt
+
+# /bin/cat out.tsv
+# /bin/cat out.txt >&2
