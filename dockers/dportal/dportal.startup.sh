d0839be2ec9f3791e453f96ef3354b73c356ee0f
Toby Batch <tobias@neontribe.co.uk>
2017-09-29 20:26:20 +0100

    Rebuilt dportal docker

--- dockers/dportal/dportal.startup.sh
+++ dockers/dportal/dportal.startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH=/root/.nvm/versions/node/v8.5.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 cd /opt/D-Portal
 ctrack/watch 1>&2 &
 
--- dockers/dportal/nvmsetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm install 8.5.0
-nvm alias default 8.5.0
-npm install -g npm@3.5.2
-ln -s /root/.nvm/versions/node/v8.5.0/bin/npm /usr/local/bin/npm
